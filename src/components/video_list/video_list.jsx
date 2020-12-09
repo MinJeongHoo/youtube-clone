50b20cb94fa24ea0d08bc3bc6b5e030c62d2ad08
MinJeongHoo <msw1302@gmail.com>
2020-12-09 16:09:10 +0900

    test

---
+++ src/components/video_list/video_list.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import VideoItem from '../video_item/video_item';
+import styles from './video_list.module.css';
+
+const Videolist = ({ videos, onVideoClick, display }) => {
+    return (
+        <ul className={styles.videos}>
+            {videos.map(video => {
+                return <VideoItem video={video} key={video.id} onVideoClick={onVideoClick} display={display} />
+            })}
+        </ul>
+    );
+};
+
+export default Videolist;
